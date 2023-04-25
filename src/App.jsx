c7b7c7a265fe0ecdaffcda6b6f5c2d5f855621ec
NicoFJCruz <nicofjcruz@gmail.com>
2023-04-25 20:23:44 -0300

    navbar

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,41 @@
+import { BrowserRouter } from "react-router-dom";
+import {
+  Hero,
+  Navbar,
+  About,
+  Tech,
+  Experience,
+  Works,
+  Feedbacks,
+  Contact,
+  EarthCanvas,
+  BallCanvas,
+  ComputersCanvas,
+  StarsCanvas,
+} from "./components";
+
+// bg-hero-pattern
+
 function App() {
   return (
-    <div>
-      Portfolio Nicolás Cruz!
-    </div>
-  )
+    <BrowserRouter>
+      <div className="relative z-0 bg-primary">
+        <div className="bg-cover bg-np-repeat bg-center">
+          <Navbar />
+          <Hero />
+        </div>
+        <About />
+        <Experience />
+        <Tech />
+        <Works />
+        <Feedbacks />
+        <div className="relative z-0">
+          <Contact />
+          <StarsCanvas />
+        </div>
+      </div>
+    </BrowserRouter>
+  );
 }
 
-export default App
+export default App;
--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,9 +1,51 @@
-import React from 'react'
+import React, { useEffect, useState } from "react";
+import { Link } from "react-router-dom";
+import { styles } from "../styles";
+import { navLinks } from "../constants";
+import { logo, menu, close } from "../assets";
 
 const Navbar = () => {
-  return (
-    <div>Navbar</div>
-  )
-}
+  const [active, setActive] = useState("");
 
-export default Navbar
+  const handleClickNavbar = () => {
+    setActive("");
+    window.scrollTo(0, 0);
+  };
+
+  return (
+    <nav
+      className={`${styles.paddingX} fixed flex w-full items-center py-5 top-0 z-20 bg-primary`}
+    >
+      <div className="flex justify-between flex items-center mas-w7xl mx-auto w-full">
+        <Link
+          to="/"
+          className="flex items-center gap-2"
+          onClick={handleClickNavbar}
+        >
+          <img className="w-9 h-9 object-contain" src={logo} alt="logo" />
+          <p className="text-white text-[18px] font-bold cursor-pointer flex">
+            Nicolás Cruz &nbsp; 
+            <span className="sm:block hidden">| FullStack Developer </span>
+          </p>
+        </Link>
+        <ul className="list-none sm:flex flex-row gap-10">
+          {navLinks.map((nav) => {
+            return (
+              <li
+                key={nav.id}
+                className={`${
+                  active === nav.title ? "text-white" : "text-secondary"
+                } hover:text-white text-[18px] font-medium cursor-pointer`}
+                onClick={() => setActive(nav.title)}
+              >
+                <a href={`#${nav.id}`}> {nav.title}</a>
+              </li>
+            );
+          })}
+        </ul>
+      </div>
+    </nav>
+  );
+};
+
+export default Navbar;
