6b52f2ee550e83b8bdb420e1afcec4edb3ecdf66
NicoFJCruz <nicofjcruz@gmail.com>
2023-04-27 20:12:18 -0300

    add ServiceCard

---
+++ src/commons/ServiceCard.jsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+const ServiceCard = ({ index, title, icon }) => {
+  return <p> {title} </p>
+};
+
+export default ServiceCard;
--- src/components/About.jsx
+++ src/components/About.jsx
@@ -1,9 +1,39 @@
-import React from 'react'
+import React from "react";
+import { Tilt } from "react-tilt";
+import { motion } from "framer-motion";
+import { styles } from "../styles";
+import { services } from "../constants";
+import { fadeIn, textVariant } from "../utils/motion";
+import ServiceCard from "../commons/ServiceCard";
 
 const About = () => {
   return (
-    <div>About</div>
-  )
-}
+    <>
+      <motion.div variants={textVariant()}>
+        <p className={styles.sectionSubText}> Introduction </p>
+        <h2 className={styles.sectionHeadText}> Overview </h2>
+      </motion.div>
 
-export default About
+      <motion.p
+        variants={fadeIn("", "", 0.1, 1)}
+        className="mt-4 text-secondary text-[17px] max-w-3xl leading-[30px]"
+      >
+        Siempre he sentido gran interés por el mundo de la informática y la
+        tecnología. Estudiando Ingeniería Electromecánica nunca dejé de lado esa
+        pasión y he asistido a pequeños cursos relacionados. Realicé un Coding
+        Bootcamp, dedicándome diariamente mas de 10 horas a profundizar en
+        nuevas habilidades técnicas y blandas. Ahora, mi objetivo es trabajar en
+        el rubro IT, donde pueda seguir aprendiendo y avanzando en este campo
+        que tanto disfruto y en el que siempre he querido estar.
+      </motion.p>
+
+      <div className="mt-20 flex flex-wrap gap-10">
+        {services.map((service, i) => {
+          return <ServiceCard key={service.title} index={i} {...service} />;
+        })}
+      </div>
+    </>
+  );
+};
+
+export default About;
