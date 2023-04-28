c24a1483087343fdd77e18ab29e2e622cbad1fa1
NicoFJCruz <nicofjcruz@gmail.com>
2023-04-28 20:57:05 -0300

    Experience Card

--- src/components/Experience.jsx
+++ src/components/Experience.jsx
@@ -1,9 +1,33 @@
-import React from 'react'
+import React from "react";
+import {
+  VerticalTimeline,
+  VerticalTimelineElement,
+} from "react-vertical-timeline-component";
+import { motion } from "framer-motion";
+import "react-vertical-timeline-component/style.min.css";
+import { styles } from "../styles";
+import { experiences } from "../constants";
+import { SectionWrapper } from "../hoc";
+import { fadeIn, textVariant } from "../utils/motion";
+import ExperienceCard from "../commons/ExperienceCard";
 
 const Experience = () => {
   return (
-    <div>Experience</div>
-  )
-}
+    <>
+      <motion.div variants={textVariant()}>
+        <p className={styles.sectionSubText}> Que hice hasta ahora </p>
+        <h2 className={styles.sectionHeadText}> Experiencia </h2>
+      </motion.div>
 
-export default Experience
+      <div className="mt-20 flex flex-col">
+        <VerticalTimeline>
+          {experiences.map((experience, i) => (
+            <ExperienceCard key={i} experience={experience}/>
+          ))}
+        </VerticalTimeline>
+      </div>
+    </>
+  );
+};
+
+export default SectionWrapper(Experience, "experience");
