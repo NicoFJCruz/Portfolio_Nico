436c0cbe56c1183a116eb613d92faa6d83117a66
NicoFJCruz <nicofjcruz@gmail.com>
2023-04-28 21:42:19 -0300

    Tech Balls

--- src/components/Tech.jsx
+++ src/components/Tech.jsx
@@ -1,9 +1,17 @@
-import React from 'react'
+import React from "react";
+import { BallCanvas } from "./canvas";
+import { SectionWrapper } from "../hoc";
+import { technologies } from "../constants";
 
 const Tech = () => {
   return (
-    <div>Tech</div>
-  )
-}
+  <div className="flex flex-row flex-wrap justify-center gap-10">
+    {technologies.map((tec, i) => (
+      <div className=" w-28 h-28" key={i}>
+        <BallCanvas icon={tec.icon} />
+      </div>
+    ))}
+  </div>);
+};
 
-export default Tech
+export default SectionWrapper(Tech, "tech");
